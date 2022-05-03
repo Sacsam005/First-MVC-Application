bd14df611ec74cd3adec893aa186abcd047bbbdb
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-05-03 12:09:22 -0500

    first commit

---
+++ First MVC Application/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using First_MVC_Application.Models;
+
+namespace First_MVC_Application.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+
+/*
+            DogViewModel doggo = new DogViewModel()
+            {
+                Name = "Tom",
+                Age = 2
+            };*/
+            return View(dogs);
+        }
+      
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult RegisterDog(DogViewModel dogViewModel)
+        {
+            /*return View("Index");*/
+
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ First MVC Application/Models/DogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace First_MVC_Application.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
